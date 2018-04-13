76ad774a5d4e96ab03866733a4100d85e1509a16
Oliver Dietz <olli.dietz@web.de>
2018-04-13 16:03:59 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,9 @@
 <?php
-    echo "Hallo "Welt"";
+echo "Wir fangen mit PHP Code an.<br>";
+?>
+    <hr>
+    <b>Hier steht plötzlich HTML Code</b><br>
+    <hr>
+<?php
+echo "Und hier steht wieder PHP Code.<br>";
+?>
