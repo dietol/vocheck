430a038fde149dd36e308ce66a7b301811aeeac2
Oliver Dietz <olli.dietz@web.de>
2018-04-13 17:09:25 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,24 @@
 <?php
-    print_r($_SERVER);
+session_start();
+
+$_logindaten = ARRAY("name"=>"admin", "passwort"=>"12345");
+
+if (isset($_POST["loginname"]) && isset($_POST["loginpasswort"]))
+{
+    if ($_logindaten["name"] == $_POST["loginname"] &&
+        $_logindaten["passwort"] == $_POST["loginpasswort"])
+    {
+        # Userdaten korrekt - User ist eingeloggt
+        # Login merken !
+        $_SESSION["login"] = 1;
+    }
+}
+
+if ($_SESSION["login"] != 1)
+{
+    include("login-formular.html");
+    exit;
+}
+
+echo "User is logged in"
+?>
