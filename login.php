0d97fbb2d61256166c34e86c30cb9fad664b72af
Oliver Dietz <olli.dietz@web.de>
2018-04-14 19:18:51 +0300

    Adjust login function

--- login.php
+++ login.php
@@ -63,6 +63,7 @@ if (!empty($_POST["submit-login"])) {
 // Check if user is logged in
 echo isset($_SESSION["login"]);
 echo $_SESSION["login"];
+echo "test";
 if (!isset($_SESSION["login"]) || $_SESSION["login"] != 1) {
     echo "i am in";
     // User is not logged in -> show login and exit
