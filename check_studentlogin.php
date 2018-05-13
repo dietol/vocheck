e6eb41f5c5430f10dd3374a8c5bef6151a22e9a6
Oliver Dietz <olli.dietz@web.de>
2018-05-13 20:15:09 +0300

    Add student FE

---
+++ check_studentlogin.php
@@ -0,0 +1,18 @@
+<?php
+
+SESSION_START();
+
+// checks if there is a user logged in and if the user is a student
+if (!isset($_SESSION["login"]) || $_SESSION["login"] != 1) {
+    // User is not logged in -> show error and exit
+    include("error.php");
+    exit;
+}
+
+if (!isset($_SESSION["account_type"]) || $_SESSION["account_type"] != 2) {
+    // User is not student -> show error and exit
+    include("error.php");
+    exit;
+}
+
+// Yes, user is logged in and student
--- student_home.php
+++ student_home.php
@@ -1,7 +1,59 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: dietola
- * Date: 13.05.18
- * Time: 14:03
- */
+include('check_studentlogin.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>vocheck</title>
+
+    <?php
+    include('static/head-imports.html');
+    ?>
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="student_home.php">vocheck</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="student_home.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="student_vocheck.php">Let's vocheck</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="student_statistics.php">Statistics</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="student_settings.php">Settings</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li>
+                <form method="POST" action="logout.php">
+                    <input type=submit name=submit-logout class="btn btn-outline-light" value="Logout">
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="row">
+    <div class="col-xl-8 offset-xl-2 col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-12">
+        <h1>vocheck</h1>
+        <h2>Your student Account - Home</h2>
+
+    </div>
+</div>
+
+</body>
+</html>
