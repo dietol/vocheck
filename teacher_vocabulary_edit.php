de6f00da078ef5c56c151e4d171517e6ddef9e1d
Oliver Dietz <olli.dietz@web.de>
2018-06-03 15:51:54 +0300

    Add Vocabulary Add Functionality

--- teacher_vocabulary_edit.php
+++ teacher_vocabulary_edit.php
@@ -12,18 +12,17 @@ switch ($_POST["op"]) {
     case "edit":
         $_listid = $_POST['id'];
 
-        $_sql = "SELECT id, meaning_first_language AS mfl, meaning_second_language AS msl FROM (SELECT vocabulary FROM vocabulary_lists WHERE deleted=0 AND list={$_listid}) AS temp LEFT JOIN vocabulary ON temp.vocabulary=vocabulary.id WHERE vocabulary.deleted=0";
+        $_sql = "SELECT id, meaning_first_language AS mfl, meaning_second_language AS msl FROM (SELECT vocabulary FROM vocabulary_lists WHERE deleted=0 AND list={$_listid}) AS temp LEFT JOIN vocabulary ON temp.vocabulary=vocabulary.id WHERE vocabulary.deleted=0 ORDER BY id DESC";
 
         if (!$_res = mysqli_query($conn, $_sql)) {
             echo "Error: %s\n" . mysqli_sqlstate($conn);
         }
 
-        $_vocs_str = "<li class=\"list-group-item d-flex justify-content-between align-items-center\" id=\"vocab-new\"><form method=\"POST\" action=\"teacher_vocabulary_edit_vocab.php\" id=\"vocab_form-new\" class=\"vocabulary_entry\"><label for=\"vocabulary_first-new\" hidden > Nothing</label ><input type=\"text\" name=\"vocabulary_first\" id=\"vocabulary_first-new\"  value=\"New Vocabulary\" /><label for=\"vocabulary_second-new\" hidden > Nothing</label ><input type=\"text\" class=\"ml-1\" name=\"vocabulary_second\" id=\"vocabulary_second-new\" value=\"Translation\"/><input type=\"submit\" class=\"btn btn-info ml-3\" name=\"vocabulary_entry_save-new\" value=\"Add\"/ ></form></li>";
+        $_vocs_str = "<li class=\"list-group-item d-flex justify-content-between align-items-center\" id=\"vocab-new\"><form method=\"POST\" action=\"teacher_vocabulary_edit_vocab.php\" id=\"vocab_form-new\" class=\"vocabulary_entry\"><label for=\"vocabulary_list\" hidden >Nothing</label><input type =\"text\" name = \"vocabulary_list\" id = \"vocabulary_list\"  value = \"{$_listid}\" hidden /><label for=\"vocabulary_first-new\" hidden > Nothing</label ><input type=\"text\" name=\"vocabulary_first\" id=\"vocabulary_first-new\"  value=\"New Vocabulary\" /><label for=\"vocabulary_second-new\" hidden > Nothing</label ><input type=\"text\" class=\"ml-1\" name=\"vocabulary_second\" id=\"vocabulary_second-new\" value=\"Translation\"/><input type=\"submit\" class=\"btn btn-info ml-3\" name=\"vocabulary_entry_save-new\" value=\"Add\"/ ></form></li>";
 
         if (mysqli_num_rows($_res) > 0) {
             // output data of each row
             while($row = mysqli_fetch_assoc($_res)) {
-                //$_vocs_str .= "<li class=\"list-group-item d-flex justify-content-between align-items-center\" id=\"class-{$row["id"]}\"><table class=\"table .table-sm\"><tbody><tr><td>{$row["mfl"]}</td><td>{$row["msl"]}</td></tr></tbody></table><span><i class=\"fas fa-pencil-alt mr-3 class-edit\"></i><i class=\"fas fa-trash-alt class-delete\"></i></span></li>";
                 $_vocs_str .= "<li class=\"list-group-item d-flex justify-content-between align-items-center\" id=\"vocab-{$row["id"]}-{$_listid}\"><form method=\"POST\" action=\"teacher_vocabulary_edit_vocab.php\" class=\"vocabulary_entry\"><label for=\"vocabulary_list\" hidden >Nothing</label><input type =\"text\" name = \"vocabulary_list\" id = \"vocabulary_list\"  value = \"{$row["id"]}-{$_listid}\" hidden /><label for=\"vocabulary_first-{$row["id"]}\" hidden>Nothing</label><input type=\"text\" name=\"vocabulary_first\" id=\"vocabulary_first-{$row["id"]}\"  value=\"{$row["mfl"]}\" readonly/><label for=\"vocabulary_second-{$row["id"]}\" hidden>Nothing</label><input type=\"text\" name=\"vocabulary_second\" class=\"ml-1\" id=\"vocabulary_second-{$row["id"]}\" value=\"{$row["msl"]}\" readonly/><input type=\"submit\" class=\"btn btn-info ml-3\" id=\"vocabulary_entry_save-{$row["id"]}\" name=\"vocabulary_entry_save\" value=\"Save\"/ hidden></form><span><i class=\"fas fa-pencil-alt mr-3 vocab-edit\"></i><i class=\"fas fa-trash-alt vocab-delete\"></i></span></li>";
             }
         } else {
--- teacher_vocabulary_edit_vocab.php
+++ teacher_vocabulary_edit_vocab.php
@@ -29,11 +29,72 @@ if (!empty($_POST["vocabulary_entry_save"])) {
     $_POST["op"] = "edit";
     $_POST["id"] = $_vocabulary_listArray[1];
 
+    mysqli_close($conn);
+    include("teacher_vocabulary_edit.php");
+    exit;
+}
+
+if(!empty($_POST['vocabulary_entry_save-new'])) {
+
+    if(!isset($_POST["vocabulary_first"]) || !isset($_POST["vocabulary_second"])) {
+        echo "Error while updating. Can not read new values.";
+        // Close Database
+        mysqli_close($conn);
+        include("teacher_vocabulary.php");
+        exit;
+    }
+
+    $_vocabulary_first = mysqli_real_escape_string($conn, $_POST["vocabulary_first"]);
+    $_vocabulary_second = mysqli_real_escape_string($conn, $_POST["vocabulary_second"]);
+    $_vocabulary_list = mysqli_real_escape_string($conn, $_POST["vocabulary_list"]);
+
+    $_sql = "SELECT first_language, second_language FROM lists WHERE id='{$_vocabulary_list}' AND deleted = 0";
+
+    if (!$_res = mysqli_query($conn, $_sql)) {
+        echo "Error: %s\n" . mysqli_sqlstate($conn);
+    }
+
+    if (mysqli_num_rows($_res) == 1) {
+        // output data of each row
+        $row = mysqli_fetch_assoc($_res);
+
+    } else {
+        echo "Error while inserting. Cannot find list.";
+        // Close Database
+        mysqli_close($conn);
+        exit;
+    }
+
+    $_sql = "INSERT INTO vocabulary (meaning_first_language, meaning_second_language, first_language, second_language) VALUES ('{$_vocabulary_first}', '{$_vocabulary_second}', '{$row['first_language']}', '{$row['second_language']}')";
+
+    if (!mysqli_query($conn, $_sql)) {
+        echo "Error while inserting. Problems with creating new vocabulary.";
+        // Close Database
+        mysqli_close($conn);
+        exit;
+    }
+
+    $_vocid = mysqli_insert_id($conn);
+
+    $_sql = "INSERT INTO vocabulary_lists (vocabulary, list) VALUES ('{$_vocid}','{$_vocabulary_list}')";
+
+    if (!mysqli_query($conn, $_sql)) {
+        echo "Error while inserting. Problems with adding vocabulary to list.";
+        // Close Database
+        mysqli_close($conn);
+        exit;
+    }
+
+    $_POST["op"] = "edit";
+    $_POST["id"] = $_vocabulary_list;
+
+    mysqli_close($conn);
     include("teacher_vocabulary_edit.php");
     exit;
 }
 
 if (!isset($_POST['op']) || !isset($_POST['vocid']) || !isset($_POST['listid'])) {
+    mysqli_close($conn);
     echo "Error. Invalid Call.";
     exit;
 }
@@ -42,12 +103,14 @@ if ($_POST['op'] == "delete") {
     $_sql = "UPDATE vocabulary SET deleted=1 WHERE id = '{$_POST["vocid"]}'";
     if (!mysqli_query($conn, $_sql)) {
         echo "Error: " . $_sql . "<br>" . mysqli_error($conn);
+        mysqli_close($conn);
         exit;
     }
 
     $_sql = "UPDATE vocabulary_lists SET deleted=1 WHERE vocabulary = '{$_POST["vocid"]}' AND list = '{$_POST["listid"]}' ";
     if (!mysqli_query($conn, $_sql)) {
         echo "Error: " . $_sql . "<br>" . mysqli_error($conn);
+        mysqli_close($conn);
         exit;
     }
 }
