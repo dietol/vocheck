c7fc665c1eeb98841973f0d90ee4f8041fd605a9
Oliver Dietz <olli.dietz@web.de>
2018-06-04 19:04:18 +0300

    Add vControl structure

--- teacher_vControl.php
+++ teacher_vControl.php
@@ -1,62 +1,24 @@
 <?php
 include('check_teacherlogin.php');
-?>
+include('static/connect-database.php');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>vocheck</title>
+$_sql = "SELECT temp.id AS d, temp.name AS a, languages.name AS b, temp.sl AS c FROM (SELECT classes.id AS id, classes.name AS name, classes.first_language AS fl, languages.name AS sl FROM classes LEFT JOIN languages ON classes.second_language = languages.id WHERE classes.deleted = 0 AND classes.teacher = {$_SESSION["user"]["id"]}) AS temp LEFT JOIN languages ON temp.fl = languages.id";
+if (!$_res = mysqli_query($conn, $_sql)) {
+    echo "Error: %s\n" . mysqli_sqlstate($conn);
+}
 
-    <?php
-    include('static/head-imports.html');
-    ?>
+$_classes_str = "";
 
-</head>
-<body>
+if (mysqli_num_rows($_res) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($_res)) {
+        $_classes_str .= "<li class=\"list-group-item d-flex justify-content-between align-items-center\" id=\"class-{$row["d"]}\"><a href='toClassStat'>{$row["a"]}</a><span class=\"mr-5\">{$row["b"]} - {$row["c"]}</span></li>";
+    }
+} else {
+    $_classes_str = "Error";
+}
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="teacher_home.php">vocheck</a>
+mysqli_close($conn);
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="teacher_home.php">Home</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="teacher_vControl.php">vocheck Control</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="teacher_vocabulary.php">Vocabulary</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="teacher_classes.php">Classes</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="teacher_settings.php">Settings</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            <li>
-                <form method="POST" action="logout.php">
-                    <input type=submit name=submit-logout class="btn btn-outline-light" value="Logout">
-                </form>
-            </li>
-        </ul>
-    </div>
-</nav>
-
-<div class="row">
-    <div class="col-xl-8 offset-xl-2 col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-12">
-        <h1>vocheck</h1>
-        <h2>Your teacher Account - vocheck Control</h2>
-
-    </div>
-</div>
-
-</body>
-</html>
+include("teacher_vControl_page.php");
