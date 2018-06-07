9916a921945a6e6fb5b75a339ea2b496a8d9f37b
Oliver Dietz <olli.dietz@web.de>
2018-06-07 18:44:59 +0300

    Add grey color for deactivated lists in vControl

--- teacher_vControl_class.php
+++ teacher_vControl_class.php
@@ -72,13 +72,31 @@ if (count($lists) > 0) {
             exit;
         }
 
+        $_sql = "SELECT status FROM lists WHERE id={$list["lid"]} AND deleted=0";
+
+        if (!$_res = mysqli_query($conn, $_sql)) {
+            echo "Error: %s\n" . mysqli_sqlstate($conn);
+        }
+
+        if (mysqli_num_rows($_res) == 1) {
+            $row = mysqli_fetch_assoc($_res);
+            if ($row["status"] == 1) {
+                $_list_class = "list_activated";
+            } else {
+                $_list_class = "list_deactivated";
+            }
+        } else {
+            echo "Error in getting status of list!";
+            exit;
+        }
+
         // Calculation Percentages
         $_total_voc = $_num_unseen + $_num_incorrect + $_num_correct;
         $_num_unseen = round(($_num_unseen * 100) / $_total_voc, 2);
         $_num_incorrect = round(($_num_incorrect * 100) / $_total_voc, 2);
         $_num_correct = round(($_num_correct * 100) / $_total_voc, 2);
 
-        $_lists_str .= "<li class=\"list-group-item\" id=\"list-{$list["lid"]}-{$_cid}\"><div class=\"row\"><div class=\"col-6\"><a href='toListStat'>{$list["lname"]}</a></div><div class=\"col-2\"><span class=\"voc_num_unseen\">{$_num_unseen} %</span></div><div class=\"col-2\"><span class=\"voc_num_incorrect\">{$_num_incorrect} %</span></div><div class=\"col-2\"><span class=\"voc_num_correct\">{$_num_correct} %</span></div></div></li>";
+        $_lists_str .= "<li class=\"list-group-item {$_list_class}\" id=\"list-{$list["lid"]}-{$_cid}\"><div class=\"row\"><div class=\"col-6\"><a href='toListStat'>{$list["lname"]}</a></div><div class=\"col-2\"><span class=\"voc_num_unseen\">{$_num_unseen} %</span></div><div class=\"col-2\"><span class=\"voc_num_incorrect\">{$_num_incorrect} %</span></div><div class=\"col-2\"><span class=\"voc_num_correct\">{$_num_correct} %</span></div></div></li>";
     }
 } else {
     $_students_str = "No Lists";
